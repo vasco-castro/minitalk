04447486ed62a9a4ff05b547c1548123a020f4d1
Vasco Castro <vsoares-@c2r7s6.42lisboa.com>
2025-11-06 18:51:39 +0000

    Change it to print only at the end, allocating memory fo the string, and sending the size of the string first

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 20:41:39 by vsoares-          #+#    #+#             */
-/*   Updated: 2025/11/03 20:16:21 by vsoares-         ###   ########.fr       */
+/*   Updated: 2025/11/06 18:39:32 by vsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,39 @@ static void	handle_args(int argc, char const *argv[])
 	if (argc != 3)
 	{
 		if (argc < 3)
-			write(1, "Error: Not enough arguments.\n", 29);
+			write(2, "Error: Not enough arguments.\n", 29);
 		else
-			write(1, "Error: Too many arguments.\n", 27);
-		write(1, "Usage: ./client <PID> <message>\n", 32);
+			write(2, "Error: Too many arguments.\n", 27);
+		write(2, "Usage: ./client <PID> <message>\n", 32);
 		exit(EXIT_FAILURE);
 	}
 	g_message.pid = ft_atoi(argv[1]);
 	if (g_message.pid <= 0 || kill(g_message.pid, 0))
 	{
-		write(1, "Error: Invalid PID.\n", 21);
+		write(2, "Error: Invalid PID.\n", 21);
 		exit(EXIT_FAILURE);
 	}
 	if (!argv[2] || argv[2][0] == '\0')
 	{
-		write(1, "Error: Message cannot be empty.\n", 33);
+		write(2, "Error: Message cannot be empty.\n", 33);
 		exit(EXIT_FAILURE);
 	}
 	g_message.msg = (char *)argv[2];
 	g_message.size = ft_strlen(argv[2]);
 }
 
-static void	send_bit()
+static void	send_size(void)
+{
+	static int	bit = 0;
+
+	if (g_message.size >> bit & 1)
+		kill(g_message.pid, SIGUSR2);
+	else
+		kill(g_message.pid, SIGUSR1);
+	bit++;
+}
+
+static void	send_char(void)
 {
 	static int	i = 0;
 	static int	bit = 0;
@@ -57,18 +68,43 @@ static void	send_bit()
 	bit++;
 }
 
-static void	finish()
+static void	finish(void)
 {
 	write(1, "Message sent successfully.\n", 27);
 	exit(EXIT_SUCCESS);
 }
 
+void	handle_sigaction(int sig, siginfo_t *info, void *context)
+{
+	static int	size = 0;
+
+	(void)info;
+	(void)context;
+	if (sig == SIGUSR2)
+		finish();
+	if(size < 32)
+	{
+		send_size();
+		size++;
+	}
+	else
+		send_char();
+}
+
 int	main(int argc, char const *argv[])
 {
+	struct sigaction	sa;
+
 	handle_args(argc, argv);
-	signal(SIGUSR1, send_bit);
-	signal(SIGUSR2, finish);
-	send_bit();
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &handle_sigaction;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	handle_sigaction(SIGUSR1, NULL, NULL);
 	while (42)
 		pause();
 	return (EXIT_SUCCESS);
--- src/server.c
+++ src/server.c
@@ -6,54 +6,81 @@
 /*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 20:40:33 by vsoares-          #+#    #+#             */
-/*   Updated: 2025/10/24 16:03:25 by vsoares-         ###   ########.fr       */
+/*   Updated: 2025/11/06 18:48:12 by vsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-// t_message	g_message;
+t_message	g_message;
+
+void	print_message()
+{
+	ft_printf("%s", g_message.msg);
+	kill(g_message.pid, SIGUSR2);
+	// ft_bzero(g_message.msg, g_message.size + 1);
+	free(g_message.msg);
+	g_message.size = 0;
+	g_message.pid = 0;
+}
+
+void	setup_msg()
+{
+	g_message.msg = ft_calloc(g_message.size + 1, sizeof(char));
+	if (!g_message.msg)
+	{
+		ft_fprintf(2,"Error with allocation!\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	parse_size(int sig)
+{
+	static int	bit = 0;
+
+	if (sig == SIGUSR2)
+		g_message.size += (1 << bit);
+	bit++;
+	kill(g_message.pid, SIGUSR1);
+}
+
+void	parse_char(int sig)
+{
+	static int	bit = 0;
+	static int	i = 0;
+
+	if (sig == SIGUSR2)
+		g_message.msg[i] += (1 << bit);
+	bit++;
+	if (bit == CHAR_BIT)
+	{
+		i++;
+		bit = 0;
+	}
+	if(i == g_message.size)
+	{
+		i = 0;
+		print_message();
+	}
+	else
+		kill(g_message.pid, SIGUSR1);
+}
 
 void	handle_sigaction(int sig, siginfo_t *info, void *context)
 {
-	static char	c = 0;
 	static int	bit = 0;
 
-	if (sig == SIGUSR2)
-		c += (1 << bit);
+	if(bit == 0)
+		g_message.pid = info->si_pid;
+	if (bit < 32)
+		parse_size(sig);
+	if (bit == 32)
+		setup_msg();
+	if (bit >= 32)
+		parse_char(sig);
 	bit++;
-	if (bit >= CHAR_BIT)
-	{
-		if (c == '\0')
-		{
-			write(1, "\n", 1);
-			kill(info->si_pid, SIGUSR2);
-		}
-		else
-			write(1, &c, 1);
-		c = 0;
+	if (bit > 32 && g_message.size == 0)
 		bit = 0;
-	}
-	kill(info->si_pid, SIGUSR1);
-}
-
-void	handle_signal(int sig)
-{
-	static char	c = 0;
-	static int	bit = 0;
-
-	if (sig == SIGUSR2)
-		c += (1 << bit);
-	bit++;
-	if (bit >= CHAR_BIT)
-	{
-		if (c == '\0')
-			write(1, "\n", 1);
-		else
-			write(1, &c, 1);
-		c = 0;
-		bit = 0;
-	}
 }
 
 int	main(void)
@@ -65,9 +92,9 @@ int	main(void)
 	sigemptyset(&sa.sa_mask);
 	sigaddset(&sa.sa_mask, SIGUSR1);
 	sigaddset(&sa.sa_mask, SIGUSR2);
-	ft_printf("Server PID: %d\n", getpid());
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("Server PID: %d\n", getpid());
 	while (42)
 		pause();
 	return (EXIT_SUCCESS);
